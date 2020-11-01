2bcc3043521131c141e010141195c1db5d7c2683
ahmetkaan <ahmetkaan@sabanciuniv.edu>
2020-11-01 11:28:36 +0300

    Some frontend

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO `Users` (Id, UserName)
+VALUES (1, "John");
+
+INSERT INTO `Users` (Id, UserName)
+VALUES (2, "Marry");
+
+INSERT INTO `Users` (Id, UserName)
+VALUES (3, "Pip");
