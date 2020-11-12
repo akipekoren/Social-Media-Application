99ad6cf58e877e39bc8c9fc06fb3713256a1f1ea
ahmetkaan <ahmetkaan@sabanciuniv.edu>
2020-11-12 15:06:23 +0300

    Main Template

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -14,7 +16,7 @@ export class NavComponent implements OnInit {
 
 
 
-  constructor(public accountService : AccountService) { }
+  constructor(public accountService : AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     
@@ -25,14 +27,16 @@ export class NavComponent implements OnInit {
 
     this.accountService.login(this.model).subscribe(response =>{
       console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error("Username or Password is invalid");
     })
   }
 
   logout() {
     this.accountService.logout();
-
+    this.router.navigateByUrl('/');
 
   }
 
